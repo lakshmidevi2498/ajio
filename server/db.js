fee462dedc0b1dad14be420f3c7b98933cbd9acd
lakshmidevi2498 <lakshmi@Actimize.in>
2025-01-17 12:27:49 +0530

    mongouri

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
  
-mongoose.connect('mongodb://localhost:27017/AJIO', {
+mongoose.connect(process.env.MONGO_URI, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 });
